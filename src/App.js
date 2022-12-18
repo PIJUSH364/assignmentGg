f2ba0c5e861d703dfbff47b954c56e8600d7b7f7
PIJUSH364 <praymondal@gmail.com>
2022-12-19 03:51:07 +0530

    readme updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { AnalyticsPage } from './components/AnalyticsPage';
 import { useResource } from './components/customHooks/useResource';
+import { resourceData } from './components/resourceData';
 
 function App() {
   const [dateObj, setDateObj] = useState({ startDate: '', endDate: '' });
-  
+
   // api does not work properly
-  const resourceData = useResource(
-    `http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03`
-  );
+  // const resourceData = useResource(
+  //   `http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03`
+  // );
   // console.log(resourceData);
+
   return (
     <div className="App">
       {resourceData ? (
